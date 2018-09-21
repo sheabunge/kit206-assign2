d1d8e1b79c70d434bbe070c0af2b4059cfe0522b
Shea Bunge <sbunge@utas.edu.au>
2018-09-21 08:17:25 +0000

    Implement methods in Database Adapter class
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@14 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

--- HRIS/Program.cs
+++ HRIS/Program.cs
@@ -1,14 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using HRIS.Database;
 
 namespace HRIS {
     class Program {
         static void Main(string[] args) {
             
-            Console.WriteLine("Hello");
+            var db = new DatabaseAdapter();
+
+            var units = db.FetchUnits();
+            
+            var classes = db.FetchClasses(units.Single(unit => unit.Code == "KIT107"));
+
+            foreach (var unitclass in classes) {
+                Console.WriteLine(unitclass.Room);
+            }
         }
     }
 }
--- HRIS/Teaching/UnitClass.cs
+++ HRIS/Teaching/UnitClass.cs
@@ -1,8 +1,16 @@
 ﻿namespace HRIS.Teaching {
+	public enum UnitClassType {
+		Tutorial,
+		Lecture,
+	}
+
 	public class UnitClass : Event {
-		
+		public UnitClassType Type { get; set; }
+
 		public string Room { get; set; }
-		
+
 		public Campus Campus { get; set; }
+
+		public Staff Staff { get; set; }
 	}
 }
