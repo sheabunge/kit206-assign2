8c30a97f2b6dafab11bc4fad23cfc83888c628a5
Shea Bunge <bungeshea@gmail.com>
2018-09-21 18:03:06 +1000

    Implement methods in Database Adapter class

--- HRIS/Teaching/Staff.cs
+++ HRIS/Teaching/Staff.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HRIS.Teaching {
 	public class Staff {
@@ -9,6 +10,8 @@ namespace HRIS.Teaching {
 		public string GivenName { get; set; }
 
 		public string Title { get; set; }
+		
+		public Campus Campus { get; set; }
 
 		public string Room { get; set; }
 
@@ -17,6 +20,8 @@ namespace HRIS.Teaching {
 		public string Photo { get; set; }
 
 		public Category Category { get; set; }
+		
+		public List<Event> Consultations { get; set; }
 
 		public Availability Availability(DateTime when) {
 			return Teaching.Availability.Free;
