586b7737c0c977d6b327978e8028d5b48af3c261
Shea Bunge <bungeshea@gmail.com>
2018-10-05 01:04:53 +1000

    Add name filter to staff tab

---
+++ HRIS/View/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using HRIS.Control;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HRIS.View {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		private readonly StaffController _staffController;
+
+		public MainWindow() {
+			_staffController = (StaffController) Application.Current.FindResource("StaffController");
+			InitializeComponent();
+		}
+
+		private void SelectCategory(object sender, SelectionChangedEventArgs e) {
+			_staffController.CurrentCategoryFilter = (Teaching.Category) CategoryFilter.SelectedItem;
+			_staffController.ApplyFilters();
+		}
+
+		private void FilterByName(object sender, TextChangedEventArgs e) {
+			_staffController.CurrentNameFilter = NameFilter.Text;
+			_staffController.ApplyFilters();
+		}
+	}
+}
