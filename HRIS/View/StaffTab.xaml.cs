e6aa5c9c0d16c56a3dab258a51d1d6f637527947
Shea Bunge <sbunge@utas.edu.au>
2018-10-04 15:24:07 +0000

    Move staff tab to a separate user control
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@35 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

---
+++ HRIS/View/StaffTab.xaml.cs
@@ -0,0 +1,31 @@
+﻿using HRIS.Control;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HRIS.View
+{
+	/// <summary>
+	/// Interaction logic for StaffTab.xaml
+	/// </summary>
+	public partial class StaffTab : UserControl {
+
+		private readonly StaffController controller;
+
+		public StaffTab() {
+			controller = (StaffController) Application.Current.FindResource("StaffController");
+			InitializeComponent();
+		}
+
+		private void SelectCategory(object sender, SelectionChangedEventArgs e) {
+			controller.CurrentCategoryFilter = (Teaching.Category)CategoryFilter.SelectedItem;
+			controller.ApplyFilters();
+		}
+
+		private void FilterByName(object sender, TextChangedEventArgs e) {
+			controller.CurrentNameFilter = NameFilter.Text;
+			controller.ApplyFilters();
+		}
+	}
+}
