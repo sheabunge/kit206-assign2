e440d78cd058d95ee0fbbf04b30fc51bd050e336
Shea Bunge <sbunge@utas.edu.au>
2018-10-05 16:31:35 +0000

    Add initial content for unit tab
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@39 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

---
+++ HRIS/View/UnitTab.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using HRIS.Control;
+
+namespace HRIS.View {
+	/// <summary>
+	/// Interaction logic for UnitTab.xaml
+	/// </summary>
+	public partial class UnitTab : UserControl {
+
+		private readonly UnitController controller;
+
+		public UnitTab() {
+			controller = (UnitController) Application.Current.FindResource("UnitController");
+			InitializeComponent();
+		}
+
+		private void FilterList(object sender, TextChangedEventArgs e) {
+			controller.CurrentFilter = SearchBox.Text;
+			controller.ApplyFilter();
+		}
+	}
+}
