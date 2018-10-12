0e24fc19b042dd448e90b71320e9e2f2c8fbf62f
Shea Bunge <sbunge@utas.edu.au>
2018-10-12 15:19:56 +0000

    Move the staff details view into its own template
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@88 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

---
+++ HRIS/View/StaffDetailsView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HRIS.Control;
+using HRIS.Teaching;
+
+namespace HRIS.View {
+	/// <summary>
+	/// Interaction logic for StaffDetailsView.xaml
+	/// </summary>
+	public partial class StaffDetailsView : UserControl {
+		private readonly StaffController controller;
+
+		public StaffDetailsView() {
+			controller = (StaffController) Application.Current.FindResource("StaffController");
+			InitializeComponent();
+		}
+
+		public void SetModel(Staff staff) {
+			StaffDetails.DataContext = staff;
+		}
+	}
+}
--- HRIS/View/StaffTab.xaml.cs
+++ HRIS/View/StaffTab.xaml.cs
@@ -11,9 +11,13 @@ namespace HRIS.View {
 	public partial class StaffTab : UserControl {
 		private readonly StaffController controller;
 
+		private Panel StaffDetails;
+
 		public StaffTab() {
 			controller = (StaffController) Application.Current.FindResource("StaffController");
 			InitializeComponent();
+
+			StaffDetails = (Panel) StaffDetailsPanel.FindName("StaffDetails");
 			StaffDetails.Visibility = Visibility.Hidden;
 		}
 
