69bc69281f1dba5c940d350a8d253f702ff408d6
Shea Bunge <bungeshea@gmail.com>
2018-10-13 01:16:07 +1100

    Add unit timetables

--- HRIS/View/UnitTab.xaml.cs
+++ HRIS/View/UnitTab.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using HRIS.Control;
+using HRIS.Teaching;
 
 namespace HRIS.View {
 	/// <summary>
@@ -12,11 +13,28 @@ namespace HRIS.View {
 		public UnitTab() {
 			controller = (UnitController) Application.Current.FindResource("UnitController");
 			InitializeComponent();
+			UnitDetails.Visibility = Visibility.Hidden;
 		}
 
 		private void FilterList(object sender, TextChangedEventArgs e) {
 			controller.CurrentFilter = SearchBox.Text;
 			controller.ApplyFilter();
+		}
+
+		private void SelectUnit(object sender, SelectionChangedEventArgs e) {
+			Unit unit = (Unit) UnitsList.SelectedItem;
+
+			if (unit == null) {
+				UnitDetails.Visibility = Visibility.Hidden;
+				NoneSelected.Visibility = Visibility.Visible;
+				return;
+			}
+
+			NoneSelected.Visibility = Visibility.Hidden;
+			UnitDetails.DataContext = unit;
+			UnitDetails.Visibility = Visibility.Visible;
+
+			Timetable.ItemsSource = controller.GetClasses(unit);
 		}
 	}
 }
