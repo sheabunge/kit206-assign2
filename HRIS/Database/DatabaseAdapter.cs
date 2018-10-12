87f31eccf74882e9564f4e28709123e3f7f06ff3
Shea Bunge <sbunge@utas.edu.au>
2018-10-12 12:21:14 +0000

    Add staff details panel
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@76 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

--- HRIS/Database/DatabaseAdapter.cs
+++ HRIS/Database/DatabaseAdapter.cs
@@ -109,7 +109,6 @@ namespace HRIS.Database {
 						Day = ParseEnum<DayOfWeek>(reader.GetString("day")),
 						Start = reader.GetTimeSpan("start"),
 						End = reader.GetTimeSpan("end"),
-						<<<<<<< HEAD
 					});
 				}
 
