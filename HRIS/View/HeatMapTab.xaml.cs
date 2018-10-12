f93117de2188dc5990baafbd21acfb8be9d2316d
Shea Bunge <sbunge@utas.edu.au>
2018-10-12 12:21:18 +0000

    Remove left over merge information
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@83 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

--- HRIS/View/HeatMapTab.xaml.cs
+++ HRIS/View/HeatMapTab.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-
 namespace HRIS.View {
 	/// <summary>
 	/// Interaction logic for HeatMapTab.xaml
