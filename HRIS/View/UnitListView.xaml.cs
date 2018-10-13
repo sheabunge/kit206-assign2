12690694e0c783c29a120ad937fbcf6c04db78a1
Shea Bunge <sbunge@utas.edu.au>
2018-10-13 07:27:41 +0000

    Move units list panel into its own view template
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@95 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

---
+++ HRIS/View/UnitListView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HRIS.Control;
+using HRIS.Teaching;
+
+namespace HRIS.View {
+	/// <summary>
+	/// Interaction logic for UnitListView.xaml
+	/// </summary>
+	public partial class UnitListView : UserControl {
+		private readonly UnitController controller;
+
+		public event EventHandler UnitSelected;
+
+		public Unit SelectedUnit => (Unit) UnitsList.SelectedItem;
+
+		public UnitListView() {
+			controller = (UnitController) Application.Current.FindResource("UnitController");
+			InitializeComponent();
+		}
+
+		private void FilterList(object sender, TextChangedEventArgs e) {
+			controller.CurrentFilter = SearchBox.Text;
+			controller.ApplyFilter();
+		}
+
+		private void UnitSelectedHandler(object sender, SelectionChangedEventArgs e) {
+			UnitSelected?.Invoke(sender, e);
+		}
+	}
+}
