6bc01fc8f4cca377869d938bb4ed01e851c85631
Jarvis Carroll <jarvisc0@utas.edu.au>
2018-10-15 07:33:21 +0000

    display text in row
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@158 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

--- HRIS/Control/ColorGrid.cs
+++ HRIS/Control/ColorGrid.cs
@@ -4,10 +4,30 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace HRIS.Control {
 	class RowData {
-
+		public String Time {
+			get {
+				return "T";
+			}
+		}
+		public String[] Values {
+			get {
+				String[] values = { "A", "B", "C", "D", "E" };
+				return values;
+			}
+		}
+		public Brush[] Colours {
+			get {
+				Brush brush = null;
+				Brush[] values = { brush, brush, brush, brush, brush };
+				return values;
+			}
+		}
+		public RowData() {
+		}
 	}
 	class ColorGrid {
 		public ObservableCollection<RowData> GetRows() {
