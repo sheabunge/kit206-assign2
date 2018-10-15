25f3d9358564f1db0e467c046bfed4bc84b9949f
Jarvis Carroll <jarvisc0@utas.edu.au>
2018-10-15 07:40:52 +0000

    initialize properties thru ctor
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@159 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

--- HRIS/Control/ColorGrid.cs
+++ HRIS/Control/ColorGrid.cs
@@ -8,30 +8,21 @@ using System.Windows.Media;
 
 namespace HRIS.Control {
 	class RowData {
-		public String Time {
-			get {
-				return "T";
-			}
-		}
-		public String[] Values {
-			get {
-				String[] values = { "A", "B", "C", "D", "E" };
-				return values;
-			}
-		}
-		public Brush[] Colours {
-			get {
-				Brush brush = null;
-				Brush[] values = { brush, brush, brush, brush, brush };
-				return values;
-			}
-		}
-		public RowData() {
+		public String Time { get; private set; }
+		public String[] Values { get; private set; }
+		public Brush[] Colours { get; private set; }
+		public RowData(String time, String[] values, Brush[] colours) {
+			Time = time;
+			Values = values;
+			Colours = colours;
 		}
 	}
 	class ColorGrid {
 		public ObservableCollection<RowData> GetRows() {
-			RowData row = new RowData();
+			String[] values = { "A", "B", "C", "D", "E" };
+			Brush brush = new Brush();
+			Brush[] colours = { brush, brush, brush, brush, brush };
+			RowData row = new RowData("T", values, colours);
 			List<RowData> rows = new List<RowData>();
 			rows.Add(row);
 			return new ObservableCollection<RowData>(rows);
