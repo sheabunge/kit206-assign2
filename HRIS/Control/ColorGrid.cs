fa25c2287aa743a9eafe934fb218a288c490a21a
Jarvis Carroll <jarvisc0@utas.edu.au>
2018-10-15 03:43:30 +0000

    add silly colorgrid class
    brb
    
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@156 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

---
+++ HRIS/Control/ColorGrid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRIS.Control {
+	class RowData {
+
+	}
+	class ColorGrid {
+		public ObservableCollection<RowData> GetVisibleList() {
+			RowData row = new RowData();
+			List<RowData> rows = new List<RowData>();
+			rows.Add(row);
+			return new ObservableCollection<RowData>(rows);
+		}
+	}
+}
