0d0dafb28166c3c116a300df42c8e4ff07e0f783
Jarvis Carroll <jarvisc0@utas.edu.au>
2018-10-15 07:54:38 +0000

    colour enum that is converted into brushes
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@160 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

--- HRIS/Control/ColorGrid.cs
+++ HRIS/Control/ColorGrid.cs
@@ -7,21 +7,40 @@ using System.Threading.Tasks;
 using System.Windows.Media;
 
 namespace HRIS.Control {
+	enum CellColour {
+		Red,
+		Orange,
+		Yellow,
+	}
 	class RowData {
 		public String Time { get; private set; }
 		public String[] Values { get; private set; }
 		public Brush[] Colours { get; private set; }
-		public RowData(String time, String[] values, Brush[] colours) {
+		public RowData(String time, String[] values, CellColour[] colours) {
 			Time = time;
 			Values = values;
-			Colours = colours;
+			Colours = new Brush[colours.Length];
+			for (int i = 0; i < colours.Length; i++) {
+				Brush next = null;
+				switch (colours[i]) {
+					case CellColour.Red:
+						next = Brushes.Red;
+						break;
+					case CellColour.Orange:
+						next = Brushes.Orange;
+						break;
+					case CellColour.Yellow:
+						next = Brushes.Yellow;
+						break;
+				}
+				Colours[i] = next;
+			}
 		}
 	}
 	class ColorGrid {
 		public ObservableCollection<RowData> GetRows() {
 			String[] values = { "A", "B", "C", "D", "E" };
-			Brush brush = new Brush();
-			Brush[] colours = { brush, brush, brush, brush, brush };
+			CellColour[] colours = { CellColour.Yellow, CellColour.Yellow, CellColour.Orange, CellColour.Red, CellColour.Orange };
 			RowData row = new RowData("T", values, colours);
 			List<RowData> rows = new List<RowData>();
 			rows.Add(row);
