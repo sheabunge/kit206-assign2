67b330769af731c2d431223b725bebba4c59184b
Shea Bunge <sbunge@utas.edu.au>
2018-10-16 00:11:51 +0000

    Add missing views
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@164 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

---
+++ HRIS/View/ColorGridControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HRIS.Control;
+
+namespace HRIS.View {
+	/// <summary>
+	/// Interaction logic for ColorGridControl.xaml
+	/// </summary>
+	public partial class ColorGridControl : UserControl {
+		public static readonly DependencyProperty RowsProperty = DependencyProperty.Register(
+			"Rows", typeof(ObservableCollection<ColorGridRow>), typeof(ColorGridControl),
+			new PropertyMetadata(new ObservableCollection<ColorGridRow>(), OnRowsChanged)
+		);
+
+		public ObservableCollection<ColorGridRow> Rows {
+			get => GetValue(RowsProperty) as ObservableCollection<ColorGridRow>;
+			set => SetValue(RowsProperty, value);
+		}
+
+		public ColorGridControl() {
+			InitializeComponent();
+		}
+
+		private static void OnRowsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+			var control = (ColorGridControl) d;
+			control.OnRowsChanged(e);
+		}
+
+		private void OnRowsChanged(DependencyPropertyChangedEventArgs e) {
+			TableMap.ItemsSource = e.NewValue as ObservableCollection<ColorGridRow>;
+		}
+	}
+}
---
+++ HRIS/View/ColorGridRow.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace HRIS.View {
+	/// <summary>
+	/// Represents the colour of a grid cell
+	/// </summary>
+	public enum CellColour {
+		White,
+		Red,
+		Orange,
+		Yellow,
+	}
+
+	/// <summary>
+	/// Represents a single row of a coloured grid
+	/// </summary>
+	public class ColorGridRow {
+		/// <summary>
+		/// The time value for this row, displayed on the y-axis
+		/// </summary>
+		public string Time { get; private set; }
+
+		/// <summary>
+		/// List of values to display in the row cells
+		/// </summary>
+		public string[] Values { get; private set; }
+
+		/// <summary>
+		/// List of colours to use for the cell backgrounds
+		/// </summary>
+		public Brush[] Colours { get; private set; }
+
+		/// <summary>
+		/// Class constructor
+		/// </summary>
+		/// <param name="time"></param>
+		/// <param name="values"></param>
+		/// <param name="colours"></param>
+		public ColorGridRow(string time, string[] values, IReadOnlyList<CellColour> colours) {
+			Time = time;
+			Values = values;
+			Colours = new Brush[colours.Count];
+
+			for (var i = 0; i < colours.Count; i++) {
+				Brush next = null;
+				switch (colours[i]) {
+					case CellColour.White:
+						next = Brushes.White;
+						break;
+					case CellColour.Red:
+						next = Brushes.Red;
+						break;
+					case CellColour.Orange:
+						next = Brushes.Orange;
+						break;
+					case CellColour.Yellow:
+						next = Brushes.Yellow;
+						break;
+				}
+
+				Colours[i] = next;
+			}
+		}
+	}
+}
