a3db49f67ddab7d9371260181955af2d4d9e2f94
Jarvis Carroll <jarvisc0@utas.edu.au>
2018-10-18 06:20:20 +0000

    event table
    git-svn-id: https://subversion.cis.utas.edu.au/svn/k206h_a2_tu1100_1@178 0d15f9d1-2fcb-4edb-aa58-c021ff7eb2bb

--- HRIS/Teaching/EventTable.cs
+++ HRIS/Teaching/EventTable.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HRIS.Teaching {
 	public class EventTable {
-		public int MaxFrequency { get; set; }
 
-		public int FrequencyAt(int hour, DayOfWeek dayofweek) {
-			return 0;
+		private int[][] frequency { get; set; }
+
+		public int FrequencyAt(int hour, DayOfWeek dayofweek) => frequency[(int) dayofweek][hour];
+
+		const int HOURS = 24;
+
+		public EventTable(IEnumerable<Event> events) {
+			frequency = new int[HOURS][];
+			for (int i = 0; i < HOURS; i++) {
+				frequency[i] = new int[7];
+			}
+
+			foreach (var ev in events) {
+				for (var hour = Math.Floor(ev.Start.TotalHours); hour < Math.Ceiling(ev.End.TotalHours); hour++) {
+					frequency[(int) ev.Day][(int) hour]++;
+				}
+			}
 		}
 	}
 }
